98825308d1ac4a80a11501172de031b0d8a3eaf4
Frigo <none@none>
2013-01-15 23:24:03 +0100

    - using try-with-resources where I can

--- src/test/java/frigo/exploratory/SerializableWithNonSerializableFields.java
+++ src/test/java/frigo/exploratory/SerializableWithNonSerializableFields.java
@@ -6,31 +6,30 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.NotSerializableException;
 import java.io.ObjectOutputStream;
-import java.io.OutputStream;
 import java.io.Serializable;
-
 import org.junit.Test;
 
 public class SerializableWithNonSerializableFields {
 
-    private static class SerializableStuff implements Serializable {
+    static class SerializableStuff implements Serializable {
+
         private static final long serialVersionUID = -3176918847542358086L;
-        private NonSerializableStuff nonSerializableObject = new NonSerializableStuff();
+        NonSerializableStuff nonSerializableObject = new NonSerializableStuff();
     }
 
     private static class NonSerializableStuff {
+
+        public NonSerializableStuff () {
+        }
     }
 
     @Test(expected = NotSerializableException.class)
-    public void testCannotSerializeSerializableClassWithNonSerializableFields() throws IOException {
+    public void testCannotSerializeSerializableClassWithNonSerializableFields () throws IOException {
         SerializableStuff object = new SerializableStuff();
         File file = new File("whatever");
-        OutputStream fileStream = new FileOutputStream(file);
-        ObjectOutputStream stream = new ObjectOutputStream(fileStream);
-        try {
+        try( ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(file)) ){
             stream.writeObject(object);
-        } finally {
-            stream.close();
+        }finally{
             file.delete();
         }
     }
--- src/test/java/frigo/exploratory/SerializationDoesNotAffectStaticFields.java
+++ src/test/java/frigo/exploratory/SerializationDoesNotAffectStaticFields.java
@@ -3,7 +3,6 @@ package frigo.exploratory;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -12,19 +11,19 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-
 import org.junit.Test;
 
 public class SerializationDoesNotAffectStaticFields {
 
     private static class SerializableStuff implements Serializable {
+
         private static final long serialVersionUID = -3176918847542358086L;
         public static int staticField = 1;
         public int field = 1;
     }
 
     @Test
-    public void testDeserializationRevertsChangesToObject() throws IOException, ClassNotFoundException {
+    public void testDeserializationRevertsChangesToObject () throws IOException, ClassNotFoundException {
         SerializableStuff object = new SerializableStuff();
         byte[] byteArray = toByteArray(object);
         object.field = 2;
@@ -33,7 +32,7 @@ public class SerializationDoesNotAffectStaticFields {
     }
 
     @Test
-    public void testDeserializationDoesNotAffectStaticFields() throws IOException, ClassNotFoundException {
+    public void testDeserializationDoesNotAffectStaticFields () throws IOException, ClassNotFoundException {
         SerializableStuff object = new SerializableStuff();
         byte[] byteArray = toByteArray(object);
         SerializableStuff.staticField = 2;
@@ -41,23 +40,19 @@ public class SerializationDoesNotAffectStaticFields {
         assertThat(SerializableStuff.staticField, is(2));
     }
 
-    private Object fromByteArray(byte[] byteArray) throws IOException, ClassNotFoundException {
-        ByteArrayInputStream stream = new ByteArrayInputStream(byteArray);
-        ObjectInput in = new ObjectInputStream(stream);
-        Object result = in.readObject();
-        in.close();
-        stream.close();
-        return result;
+    private Object fromByteArray (byte[] byteArray) throws IOException, ClassNotFoundException {
+        try( ObjectInput in = new ObjectInputStream(new ByteArrayInputStream(byteArray)) ){
+            return in.readObject();
+        }
     }
 
-    private byte[] toByteArray(Serializable object) throws IOException {
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        ObjectOutput out = new ObjectOutputStream(stream);
-        out.writeObject(object);
-        byte[] result = stream.toByteArray();
-        out.close();
-        stream.close();
-        return result;
+    private byte[] toByteArray (Serializable object) throws IOException {
+        try( ByteArrayOutputStream stream = new ByteArrayOutputStream() ){
+            try( ObjectOutput out = new ObjectOutputStream(stream) ){
+                out.writeObject(object);
+            }
+            return stream.toByteArray();
+        }
     }
 
 }
