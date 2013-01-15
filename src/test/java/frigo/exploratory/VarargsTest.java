2306936db8f11d70bcf8600dc32433dba292dcda
Frigo <none@none>
2013-01-15 16:47:32 +0100

    - added exploratory test for Mockito

---
+++ src/test/java/frigo/exploratory/VarargsTest.java
@@ -0,0 +1,24 @@
+
+package frigo.exploratory;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class VarargsTest {
+
+    @Test
+    public void empty_varargs_can_be_iterated() {
+        assertThat(concat(), is(""));
+    }
+
+    private String concat(String... strings) {
+        String result = "";
+        for (String string : strings) {
+            result += string;
+        }
+        return result;
+    }
+
+}
