428ed34bb44a40a2ee94fb292b29b24b912514b3
Frigo <none@none>
2013-04-22 00:13:16 +0200

    - refactored a lot

--- src/test/java/frigo/electronics/ParallelRlcWithLoadFinderTest.java
+++ src/test/java/frigo/electronics/ParallelRlcWithLoadFinderTest.java
@@ -1,6 +1,7 @@
 
 package frigo.electronics;
 
+import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -9,25 +10,45 @@ import org.junit.Test;
 public class ParallelRlcWithLoadFinderTest {
 
     private ParallelRlcWithLoadFinder finder = new ParallelRlcWithLoadFinder(4800, -10, 0.3, 32);
+    private ParallelRlcWithLoad rlc = finder.getFilter();
 
     @Test
-    public void testR () {
-        assertThat(finder.R, is(69.19288512538813));
+    public void generated_RLC_filter_has_proper_f0 () {
+        assertThat(rlc.f0(), closeTo(4800.0, 1E-12));
     }
 
     @Test
-    public void testLC () {
-        assertThat(finder.LC, is(1.0994052044524501E-9));
+    public void generated_RLC_filter_has_proper_f1 () {
+        assertThat(rlc.f1(), is(1329.5230317524806));
     }
 
     @Test
-    public void testLowerCutoffFrequency () {
-        assertThat(finder.f1, is(1329.5230317524802));
+    public void generated_RLC_filter_has_proper_f2 () {
+        assertThat(rlc.f2(), is(17329.523031752477));
     }
 
     @Test
-    public void testUpperCutoffFrequency () {
-        assertThat(finder.f2, is(17329.52303175248));
+    public void generated_RLC_filter_has_proper_gain () {
+        assertThat(rlc.gain(), is(-10.0));
     }
 
+    @Test
+    public void generated_RLC_filter_has_proper_q () {
+        assertThat(rlc.q(), is(0.3));
+    }
+
+    @Test
+    public void generated_RLC_filter_has_proper_response_at_f0 () {
+        assertThat(rlc.response(rlc.f0()), closeTo(-10.0, 1E-14));
+    }
+
+    @Test
+    public void generated_RLC_filter_has_proper_response_at_f1 () {
+        assertThat(rlc.response(rlc.f1()), closeTo(-10.0 + 3.0, 1E-15));
+    }
+
+    @Test
+    public void generated_RLC_filter_has_proper_response_at_f2 () {
+        assertThat(rlc.response(rlc.f2()), closeTo(-10.0 + 3.0, 1E-15));
+    }
 }
