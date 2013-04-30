4c01db2e03361bb05b4e5ac5bcdfe2633fb53d73
Frigo <none@none>
2013-04-30 22:22:21 +0200

    - added StringJoiner dojo

---
+++ src/main/java/frigo/dojo/StringJoiner.java
@@ -0,0 +1,23 @@
+
+package frigo.dojo;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class StringJoiner {
+
+    public static String join (Collection<String> strings, String delimiter) {
+        if( strings == null ){
+            return "";
+        }
+        StringBuilder builder = new StringBuilder();
+        for( Iterator<String> iterator = strings.iterator(); iterator.hasNext(); ){
+            String string = iterator.next();
+            builder.append(string);
+            if( iterator.hasNext() ){
+                builder.append(delimiter);
+            }
+        }
+        return builder.toString();
+    }
+}
---
+++ src/test/java/frigo/dojo/StringJoinerTest.java
@@ -0,0 +1,50 @@
+
+package frigo.dojo;
+
+import static frigo.dojo.StringJoiner.join;
+import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Collection;
+
+import org.junit.Test;
+
+public class StringJoinerTest {
+
+    @Test
+    public void join_returns_empty_string_on_null () {
+        Collection<String> strings = null;
+        assertThat(join(strings, ","), is(""));
+    }
+
+    @Test
+    public void join_returns_empty_string_on_empty_collection () {
+        Collection<String> strings = null;
+        assertThat(join(strings, ","), is(""));
+    }
+
+    @Test
+    public void join_returns_empty_string_on_empty_string () {
+        Collection<String> strings = asList("");
+        assertThat(join(strings, ","), is(""));
+    }
+
+    @Test
+    public void join_leaves_a_single_string_alone () {
+        Collection<String> strings = asList("Hello World!");
+        assertThat(join(strings, ","), is("Hello World!"));
+    }
+
+    @Test
+    public void join_merges_two_strings_with () {
+        Collection<String> strings = asList("Hello", "World!");
+        assertThat(join(strings, ","), is("Hello,World!"));
+    }
+
+    @Test
+    public void join_merges_many_strings_with_space_delimiter () {
+        Collection<String> strings = asList("Hello", "World", "!");
+        assertThat(join(strings, " "), is("Hello World !"));
+    }
+}
