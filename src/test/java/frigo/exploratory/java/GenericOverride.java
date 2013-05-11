d7247ca3718686313b0fa4b0c557caf4cba51289
Frigo <none@none>
2013-05-12 00:01:32 +0200

    - simplified test, no point in instantiating stuff, a simply different return value is okay as well

--- src/test/java/frigo/exploratory/java/GenericOverride.java
+++ src/test/java/frigo/exploratory/java/GenericOverride.java
@@ -1,33 +1,31 @@
 
 package frigo.exploratory.java;
 
-import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
 public class GenericOverride {
 
-    static class Empty {
-    }
-
     private static class Base {
-        public <T> T whatever(Class<T> clazz) throws Exception {
-            return null;
+
+        public <T> T whatever (T value) {
+            return value == null ? value : null;
         }
     }
 
     private static class Sub extends Base {
+
         @Override
-        public <T> T whatever(Class<T> clazz) throws Exception {
-            return clazz.newInstance();
+        public <T> T whatever (T value) {
+            return value;
         }
     }
 
     @Test
-    public void generic_method_can_be_overridden() throws Exception {
+    public void generic_method_can_be_overridden () {
         Base source = new Sub();
-        assertThat(source.whatever(Empty.class), instanceOf(Empty.class));
+        assertThat(source.whatever(1), is(1));
     }
-
 }
