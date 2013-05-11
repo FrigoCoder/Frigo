be1430d80b1ac265fe2a2a4c817395e1e207f6ea
Frigo <none@none>
2013-05-11 23:59:29 +0200

    - removed unsued exception declarations

--- src/test/java/frigo/exploratory/AwaitVisibilityTest.java
+++ src/test/java/frigo/exploratory/AwaitVisibilityTest.java
@@ -29,17 +29,17 @@ public class AwaitVisibilityTest {
     }
 
     @Test
-    public void await_can_wait_for_protected_method () throws Exception {
+    public void await_can_wait_for_protected_method () {
         waitAtMost(10, TimeUnit.SECONDS).untilCall(to(this).protectedMethod(), is(true));
     }
 
     @Test
-    public void await_can_wait_for_package_private_method () throws Exception {
+    public void await_can_wait_for_package_private_method () {
         waitAtMost(10, TimeUnit.SECONDS).untilCall(to(this).packagePrivateMethod(), is(true));
     }
 
     @Test
-    public void await_can_not_wait_for_private_method () throws Exception {
+    public void await_can_not_wait_for_private_method () {
         try{
             waitAtMost(10, TimeUnit.SECONDS).untilCall(to(this).privateMethod(), is(true));
         }finally{
