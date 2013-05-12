9e6056d09f6e1f614fb8ac2d0f1f9db4a9c7b5d7
Frigo <none@none>
2013-05-12 16:08:03 +0200

    - removed unused graph classes

--- src/main/java/frigo/graph/EdgeMap.java
+++
@@ -1,9 +0,0 @@
-
-package frigo.graph;
-
-public interface EdgeMap<T> {
-
-    T get (Edge node);
-
-    void set (Edge node, T obj);
-}
--- src/main/java/frigo/graph/NodeMap.java
+++
@@ -1,9 +0,0 @@
-
-package frigo.graph;
-
-public interface NodeMap<T> {
-
-    T get (Node node);
-
-    void set (Node node, T obj);
-}
