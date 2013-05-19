c43a99575a99fc82994549798c84950cb23d2a35
Frigo <none@none>
2013-05-19 19:28:29 +0200

    - added p-norm

--- src/main/java/frigo/math/Statistics.java
+++ src/main/java/frigo/math/Statistics.java
@@ -1,7 +1,10 @@
 
 package frigo.math;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static frigo.math.MathAux.sqr;
+import static java.lang.Math.abs;
+import static java.lang.Math.pow;
 
 public class Statistics {
 
@@ -22,6 +25,32 @@ public class Statistics {
         }
         variance /= values.length;
         return variance;
+    }
+
+    public static double manhattanDistance (double[] v, double[] w) {
+        return norm(minus(v, w), 1);
+    }
+
+    public static double euclideanDistance (double[] v, double[] w) {
+        return norm(minus(v, w), 2);
+    }
+
+    public static double norm (double[] v, double p) {
+        double result = 0.0;
+        for( int i = 0; i < v.length; i++ ){
+            result += pow(abs(v[i]), p);
+        }
+        result = pow(result, 1.0 / p);
+        return result;
+    }
+
+    private static double[] minus (double[] v, double[] w) {
+        checkArgument(v.length == w.length);
+        double[] result = new double[v.length];
+        for( int i = 0; i < result.length; i++ ){
+            result[i] = v[i] - w[i];
+        }
+        return result;
     }
 
 }
--- src/test/java/frigo/math/StatisticsTest.java
+++ src/test/java/frigo/math/StatisticsTest.java
@@ -2,16 +2,25 @@
 package frigo.math;
 
 import static frigo.math.Statistics.average;
+import static frigo.math.Statistics.euclideanDistance;
+import static frigo.math.Statistics.manhattanDistance;
+import static frigo.math.Statistics.norm;
 import static frigo.math.Statistics.variance;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class StatisticsTest {
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     private double[] linear = {1, 2, 3, 4, 5, 6};
     private double[] quadratic = {1, 4, 9, 16, 25, 36, 49};
+    private double[] other = {1, 3, 5, 7, 9, 11};
 
     @Test
     public void average_of_linear_array () {
@@ -32,6 +41,43 @@ public class StatisticsTest {
     @Test
     public void variance_of_quadratic_array () {
         assertThat(variance(quadratic), is(1_876.0 / 7));
+    }
+
+    @Test
+    public void manhattanDistance_of_two_arrays () {
+        assertThat(manhattanDistance(linear, other), is(15.0));
+    }
+
+    @Test
+    public void manhattanDistance_should_throw_on_length_mismatch () {
+        thrown.expect(IllegalArgumentException.class);
+        manhattanDistance(linear, new double[] {});
+    }
+
+    @Test
+    public void euclideanDistance_of_two_arrays () {
+        assertThat(euclideanDistance(linear, other), is(7.4161984870956629487113974408007));
+    }
+
+    @Test
+    public void euclideanDistance_should_throw_on_length_mismatch () {
+        thrown.expect(IllegalArgumentException.class);
+        euclideanDistance(linear, new double[] {});
+    }
+
+    @Test
+    public void norm_harmonic_norm () {
+        assertThat(norm(linear, -1), is(0.4081632653061225));
+    }
+
+    @Test
+    public void norm_manhattan_norm () {
+        assertThat(norm(linear, 1), is(21.0));
+    }
+
+    @Test
+    public void norm_euclidean_norm () {
+        assertThat(norm(linear, 2), is(9.5393920141694564915262158602323));
     }
 
 }
