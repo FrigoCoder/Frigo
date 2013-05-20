f02470ffd6f549c5e3ae4f0a9539b4bd0ad173ed
Frigo <none@none>
2013-05-20 03:27:37 +0200

    - we should focus on finding R,L,C for f0,gain,Q,load values instead of trying to optimize frequency responses or emulating headphones

--- src/main/java/frigo/electronics/BruteForceParallelRlcWithLoadFinder.java
+++ src/main/java/frigo/electronics/BruteForceParallelRlcWithLoadFinder.java
@@ -1,18 +1,21 @@
 
 package frigo.electronics;
 
-import static frigo.electronics.IEC60063.E12;
+import static frigo.electronics.IEC60063.E192;
 import static frigo.electronics.IEC60063.applyDecades;
 import static frigo.electronics.Prefix.toUnit;
 import static frigo.electronics.Util.qFactorToOctaveBandwidth;
-import frigo.math.Statistics;
+import frigo.math.MathAux;
 
 public class BruteForceParallelRlcWithLoadFinder {
 
+    private double f0;
+    private double gain;
+    private double q;
+    private double load;
+
     public static void main (String[] args) {
-        String sourceFile = "HD681.txt";
-        String targetFile = "HD681EVO.txt";
-        BruteForceParallelRlcWithLoadFinder finder = new BruteForceParallelRlcWithLoadFinder(sourceFile, targetFile);
+        BruteForceParallelRlcWithLoadFinder finder = new BruteForceParallelRlcWithLoadFinder(4800, -8, 0.5, 35);
 
         ParallelRlcWithLoad rlc = finder.getBestRlc();
         log("Best: " + rlc);
@@ -26,34 +29,17 @@ public class BruteForceParallelRlcWithLoadFinder {
         System.out.println(message);
     }
 
-    private ResponseInterpolator source;
-    private ResponseInterpolator target;
-    private double load = 35;
-    private double left = 1000;
-    private double right = 16000;
-    private int samples = 100;
-    private double[] samplingFrequencies = getSamplingFrequencies();
-    private double[] targetSamples;
-
-    private double[] Rs = applyDecades(E12, new double[] {1, 10, 100});
-    private double[] Ls = applyDecades(E12, new double[] {toUnit("100u"), toUnit("1m"), toUnit("10m"), toUnit("100m")});
-    private double[] Cs = applyDecades(E12, new double[] {toUnit("1n"), toUnit("10n"), toUnit("100n"), toUnit("1m")});
-
-    public BruteForceParallelRlcWithLoadFinder (String sourceFile, String targetFile) {
-        source = new ResponseInterpolator(sourceFile);
-        target = new ResponseInterpolator(targetFile);
-        targetSamples = sampleTargetHeadphone();
+    public BruteForceParallelRlcWithLoadFinder (double f0, double gain, double q, double load) {
+        this.f0 = f0;
+        this.gain = gain;
+        this.q = q;
+        this.load = load;
     }
 
-    private double[] getSamplingFrequencies () {
-        double[] points = new double[samples];
-        double increment = (right - left) / (samples - 1);
-        points[0] = left;
-        for( int i = 1; i < points.length; i++ ){
-            points[i] = points[i - 1] + increment;
-        }
-        return points;
-    }
+    private double[] Rs = applyDecades(E192, new double[] {10});
+    private double[] Ls =
+        applyDecades(E192, new double[] {toUnit("100u"), toUnit("1m"), toUnit("10m"), toUnit("100m")});
+    private double[] Cs = applyDecades(E192, new double[] {toUnit("1n"), toUnit("10n"), toUnit("100n"), toUnit("1m")});
 
     public ParallelRlcWithLoad getBestRlc () {
         Minimum<ParallelRlcWithLoad> minimum = new Minimum<>();
@@ -72,32 +58,16 @@ public class BruteForceParallelRlcWithLoadFinder {
     }
 
     private double evaluate (ParallelRlcWithLoad rlc) {
-        // return Statistics.variance(sampleFilteredHeadphone(rlc));
-        // return Statistics.euclideanDistance(sampleFilteredHeadphone(rlc), targetSamples);
-        return Statistics.manhattanDistance(sampleFilteredHeadphone(rlc), targetSamples);
-        // return d(rlc.f0(), 4800) + d(rlc.gain(), -8) + d(rlc.q(), 0.5);
+        try{
+            return d(rlc.f0(), f0) + d(rlc.gain(), gain) + d(rlc.q(), q);
+        }catch( IllegalArgumentException e ){
+            System.out.println("Encountered too small RLC");
+            return Double.POSITIVE_INFINITY;
+        }
     }
 
-    // private double d (double actual, double expected) {
-    // return abs(expected - actual) / expected;
-    // }
-
-    private double[] sampleFilteredHeadphone (ParallelRlcWithLoad rlc) {
-        double[] sampledValues = new double[samples];
-        for( int i = 0; i < samples; i++ ){
-            double f = samplingFrequencies[i];
-            sampledValues[i] = source.response(f) + rlc.response(f);
-        }
-        return sampledValues;
-    }
-
-    private double[] sampleTargetHeadphone () {
-        double[] sampledValues = new double[samples];
-        for( int i = 0; i < samples; i++ ){
-            double f = samplingFrequencies[i];
-            sampledValues[i] = target.response(f);
-        }
-        return sampledValues;
+    private double d (double actual, double expected) {
+        return MathAux.sqr((expected - actual) / expected);
     }
 
 }
