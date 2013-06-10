d2b3177cbe366b17a454d3d1a5b599079fbdeb06
Frigo <none@none>
2013-06-11 00:16:59 +0200

    - fuck tests

---
+++ src/main/java/frigo/electronics/DeconvolutedImpulseResponseGenerator.java
@@ -0,0 +1,53 @@
+
+package frigo.electronics;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import frigo.util.ArraysAux;
+
+public class DeconvolutedImpulseResponseGenerator {
+
+    public static void main (String[] args) throws IOException {
+        // DeconvolutedImpulseResponseGenerator generator =
+        // new DeconvolutedImpulseResponseGenerator("0_96000_4096_kaiser7.txt");
+        DeconvolutedImpulseResponseGenerator generator = new DeconvolutedImpulseResponseGenerator("response.txt");
+        generator.write("output.snd");
+    }
+
+    private int fftSize;
+    private ResponseInterpolator interpolator;
+
+    private DeconvolutedImpulseResponseGenerator (String fileName) {
+        fftSize = 1024;
+        interpolator = new ResponseInterpolator(fileName);
+    }
+
+    private void write (String fileName) throws IOException {
+        double[] impulseResponse = getImpulseResponse();
+        byte[] byteArray = ArraysAux.encodeInByteArray(impulseResponse);
+        try( FileOutputStream out = new FileOutputStream(fileName) ){
+            out.write(byteArray);
+        }
+    }
+
+    private double[] getImpulseResponse () {
+        DigitalFrequencyResponse headphoneResponse = new DigitalFrequencyResponse(interpolator, fftSize);
+        DigitalFrequencyResponse correctionResponse = headphoneResponse.invert();
+        DigitalFrequencyResponse identity = headphoneResponse.convolute(correctionResponse);
+        // return normalize(correctionResponse.toShiftedKernel());
+        return normalize(identity.toShiftedKernel());
+    }
+
+    private double[] normalize (double[] v) {
+        double max = 0.0;
+        for( double value : v ){
+            max = Math.max(max, value);
+        }
+        double[] result = new double[v.length];
+        for( int i = 0; i < result.length; i++ ){
+            result[i] = v[i] / max;
+        }
+        return result;
+    }
+}
