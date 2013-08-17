15f00e0d8e1cb557db90b2d08726975fa68039dc
Frigo <none@none>
2013-08-17 21:42:58 +0200

    - added bowling dojo :)

---
+++ src/main/java/frigo/dojo/Bowling.java
@@ -0,0 +1,42 @@
+
+package frigo.dojo;
+
+public class Bowling {
+
+    private int[] rolls = new int[22];
+    private int numberOfRolls;
+
+    public void roll (int roll) {
+        rolls[numberOfRolls++] = roll;
+    }
+
+    public int score () {
+        int score = 0;
+        int i = 0;
+        for( int frame = 0; frame < 10; frame++ ){
+            score += rolls[i];
+            score += rolls[i + 1];
+            if( isStrike(i) ){
+                score += rolls[i + 2];
+                i--;
+            }else if( isSpare(i) ){
+                score += rolls[i + 2];
+            }
+            i++;
+            i++;
+        }
+        return score;
+    }
+
+    private boolean isStrike (int roll) {
+        return rolls[roll] == 10;
+    }
+
+    private boolean isSpare (int roll) {
+        if( isStrike(roll) ){
+            return false;
+        }
+        return rolls[roll] + rolls[roll + 1] == 10;
+    }
+
+}
---
+++ src/test/java/frigo/dojo/BowlingTest.java
@@ -0,0 +1,108 @@
+
+package frigo.dojo;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class BowlingTest {
+
+    private Bowling bowling = new Bowling();
+
+    @Test
+    public void total_fluke () {
+        rollMany(20, 0);
+        assertScore(0);
+    }
+
+    @Test
+    public void all_one_rolls () {
+        rollMany(20, 1);
+        assertScore(20);
+    }
+
+    @Test
+    public void all_two_rolls () {
+        rollMany(20, 2);
+        assertScore(40);
+    }
+
+    @Test
+    public void roll_after_spare_is_counted_twice () {
+        roll(0);
+        roll(10);
+        roll(1);
+        assertScore(12);
+    }
+
+    @Test
+    public void second_roll_after_spare_is_not_counted_twice () {
+        roll(0);
+        roll(10);
+        roll(1);
+        roll(1);
+        assertScore(13);
+    }
+
+    @Test
+    public void roll_after_strike_is_counted_twice () {
+        roll(10);
+        roll(1);
+        assertScore(12);
+    }
+
+    @Test
+    public void second_roll_after_strike_is_counted_twice () {
+        roll(10);
+        roll(1);
+        roll(1);
+        assertScore(14);
+    }
+
+    @Test
+    public void perfect_game () {
+        rollMany(12, 10);
+        assertScore(300);
+    }
+
+    @Test
+    public void eleven_strikes_and_zero () {
+        rollMany(11, 10);
+        roll(0);
+        assertScore(290);
+    }
+
+    @Test
+    public void ten_strikes_and_two_zeros () {
+        rollMany(10, 10);
+        roll(0);
+        roll(0);
+        assertScore(270);
+    }
+
+    @Test
+    public void strike_and_zero_is_not_mistaken_for_spare () {
+        roll(10);
+        roll(0);
+        roll(1);
+        roll(1);
+        roll(0);
+        assertScore(13);
+    }
+
+    private void rollMany (int times, int score) {
+        for( int i = 0; i < times; i++ ){
+            roll(score);
+        }
+    }
+
+    private void roll (int roll) {
+        bowling.roll(roll);
+    }
+
+    private void assertScore (int score) {
+        assertThat(bowling.score(), is(score));
+    }
+
+}
