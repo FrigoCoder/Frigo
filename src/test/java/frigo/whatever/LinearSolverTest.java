758deabc02fd39dff8c96580f8ab7fe95c3cad73
Frigo <none@none>
2014-02-05 19:42:54 +0100

    - added test for 2x2 matrix inversion

--- src/test/java/frigo/whatever/LinearSolverTest.java
+++ src/test/java/frigo/whatever/LinearSolverTest.java
@@ -19,8 +19,19 @@ public class LinearSolverTest {
         A.setRow(0, 1);
 
         Vector b = Vector.from(1);
-
-        Vector actual = solver.solve(A, b);
-        assertThat(actual, is(Vector.from(1)));
+        assertThat(solver.solve(A, b), is(Vector.from(1)));
     }
+
+    @Test
+    public void can_solve_2_by_2_problem() {
+        Matrix A = new Matrix(2, 2);
+        A.setRow(0, 1, 2);
+        A.setRow(1, 2, 3);
+
+        Vector b1 = Vector.from(1, 0);
+        Vector b2 = Vector.from(0, 1);
+        assertThat(solver.solve(A, b1), is(Vector.from(-3, 2)));
+        assertThat(solver.solve(A, b2), is(Vector.from(-2, -1)));
+    }
+
 }
