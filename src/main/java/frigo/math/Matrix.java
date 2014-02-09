92dce0b90d7a91b624dc9dbf112259791c391427
Frigo <none@none>
2014-02-09 18:44:37 +0100

    - added Matrix.identity

--- src/main/java/frigo/math/Matrix.java
+++ src/main/java/frigo/math/Matrix.java
@@ -9,38 +9,46 @@ public class Matrix {
     public final int m;
     private final Vector[] vectors;
 
-    public Matrix(int rows, int columns) {
+    public static Matrix identity (int n) {
+        Matrix matrix = new Matrix(n, n);
+        for( int i = 0; i < n; i++ ){
+            matrix.set(i, i, 1.0);
+        }
+        return matrix;
+    }
+
+    public Matrix (int rows, int columns) {
         n = rows;
         m = columns;
         vectors = new Vector[columns];
-        for (int j = 0; j < columns; j++) {
+        for( int j = 0; j < columns; j++ ){
             vectors[j] = Vector.create(rows);
         }
     }
 
-    public double get(int i, int j) {
+    public double get (int i, int j) {
         return vectors[j].get(i);
     }
 
-    public void set(int i, int j, double value) {
+    public void set (int i, int j, double value) {
         vectors[j].set(i, value);
     }
 
-    public Vector vector(int j) {
+    public Vector vector (int j) {
         return vectors[j];
     }
 
-    public void setRow(int i, double... values) {
+    public void setRow (int i, double... values) {
         Preconditions.checkArgument(m == values.length);
-        for (int j = 0; j < m; j++) {
+        for( int j = 0; j < m; j++ ){
             set(i, j, values[j]);
         }
     }
 
-    public Vector mul(Vector x) {
+    public Vector mul (Vector x) {
         Preconditions.checkArgument(m == x.n);
         Vector result = Vector.create(n);
-        for (int j = 0; j < m; j++) {
+        for( int j = 0; j < m; j++ ){
             result = result.add(vector(j).mul(x.get(j)));
         }
         return result;
--- src/test/java/frigo/math/MatrixTest.java
+++ src/test/java/frigo/math/MatrixTest.java
@@ -18,22 +18,22 @@ public class MatrixTest {
     private Matrix matrix = new Matrix(n, m);
 
     @Test
-    public void dimensions_are_correct() {
+    public void dimensions_are_correct () {
         assertThat(matrix.n, is(n));
         assertThat(matrix.m, is(m));
     }
 
     @Test
-    public void coefficients_are_zero_by_default() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+    public void coefficients_are_zero_by_default () {
+        for( int i = 0; i < n; i++ ){
+            for( int j = 0; j < m; j++ ){
                 assertThat(matrix.get(i, j), is(0.0));
             }
         }
     }
 
     @Test
-    public void sets_value_properly() {
+    public void sets_value_properly () {
         int i = 1;
         int j = 2;
         matrix.set(i, j, 1);
@@ -41,7 +41,7 @@ public class MatrixTest {
     }
 
     @Test
-    public void column_vector_is_returned() {
+    public void column_vector_is_returned () {
         int i = 1;
         int j = 2;
         matrix.set(i, j, 1);
@@ -49,7 +49,7 @@ public class MatrixTest {
     }
 
     @Test
-    public void setRow_works_properly() {
+    public void setRow_works_properly () {
         matrix.setRow(0, 1, 2, 3);
         matrix.setRow(1, 4, 5, 6);
         assertThat(matrix.vector(0), is(Vector.from(1, 4)));
@@ -58,10 +58,24 @@ public class MatrixTest {
     }
 
     @Test
-    public void multiplication_works_properly() {
+    public void multiplication_works_properly () {
         matrix.setRow(0, 1, 2, 3);
         matrix.setRow(1, 4, 5, 6);
         assertThat(matrix.mul(Vector.from(1, 2, 3)), is(Vector.from(14, 32)));
+    }
+
+    @Test
+    public void identity_matrix_is_correct () {
+        matrix = Matrix.identity(5);
+        for( int i = 0; i < matrix.n; i++ ){
+            for( int j = 0; j < matrix.m; j++ ){
+                if( i == j ){
+                    assertThat(matrix.get(i, j), is(1.0));
+                }else{
+                    assertThat(matrix.get(i, j), is(0.0));
+                }
+            }
+        }
     }
 
 }
