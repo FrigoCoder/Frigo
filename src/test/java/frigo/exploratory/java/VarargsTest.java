a56bb22a5bace3e91ca80d7d7cdc7dc933c6d040
frigocoder@gmail.com <frigocoder@gmail.com>
2016-12-31 10:51:49 +0100

    - added example for unsafe varargs

--- src/test/java/frigo/exploratory/java/VarargsTest.java
+++ src/test/java/frigo/exploratory/java/VarargsTest.java
@@ -4,9 +4,17 @@ package frigo.exploratory.java;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class VarargsTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void empty_varargs_can_be_iterated () {
@@ -19,6 +27,21 @@ public class VarargsTest {
             result += string;
         }
         return result;
+    }
+
+    @Test
+    public void unsafe_varargs_can_cause_runtime_exception () {
+        List<String> list1 = Arrays.asList("a", "b");
+        List<String> list2 = Arrays.asList("c", "d");
+        thrown.expect(ClassCastException.class);
+        unsafeVarargMethod(list1, list2);
+    }
+
+    @SafeVarargs
+    private final void unsafeVarargMethod (List<String>... stringLists) {
+        Object[] array = stringLists;
+        array[0] = Arrays.asList(42);
+        String firstString = stringLists[0].get(0);
     }
 
 }
