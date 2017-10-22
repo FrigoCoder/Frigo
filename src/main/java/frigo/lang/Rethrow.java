b99e3939751939886c9246094a8f91da02cb686e
frigocoder@gmail.com <frigocoder@gmail.com>
2017-10-22 16:15:20 +0200

    Implemented better ways to redeclare catched exceptions.

--- src/main/java/frigo/lang/Rethrow.java
+++ src/main/java/frigo/lang/Rethrow.java
@@ -1,9 +1,23 @@
 
 package frigo.lang;
 
-public class Rethrow {
+public class Rethrow implements AutoCloseable {
 
-    @SuppressWarnings("unused")
-    public static <T extends Throwable> void canThrow (Class<T> clazz) throws T {}
+    public static <E extends Exception> Rethrow canThrow (Class<E> e) throws E {
+        return new Rethrow(e);
+    }
+
+    public static <R, E extends Exception> R canThrow (R result, Class<E> e) throws E {
+        return result;
+    }
+
+    public static <E extends Exception> void canThrow (Runnable runnable, Class<E> e) throws E {
+        runnable.run();
+    }
+
+    public <E extends Exception> Rethrow (Class<E> e) throws E {}
+
+    @Override
+    public void close () {}
 
 }
--- src/test/java/frigo/lang/RethrowTest.java
+++ src/test/java/frigo/lang/RethrowTest.java
@@ -2,10 +2,11 @@
 package frigo.lang;
 
 import static frigo.lang.Rethrow.canThrow;
+import static org.junit.Assert.assertThat;
 
-import java.io.IOException;
+import java.io.FileNotFoundException;
 
-import org.junit.Assert;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import lombok.SneakyThrows;
@@ -13,18 +14,80 @@ import lombok.SneakyThrows;
 public class RethrowTest {
 
     @Test
-    public void redeclare_and_catch () {
+    public void no_need_for_declarations () {
+        sneakilyThrows();
+        intSneakilyThrows();
+    }
+
+    @Test
+    public void declarations_are_not_suppressed () {
         try{
-            canThrow(IOException.class);
+            loudlyThrows();
+        }catch( FileNotFoundException e ){
+        }
+    }
+
+    @Test
+    public void declare_exceptions () {
+        try{
+            canThrow(FileNotFoundException.class);
+        }catch( FileNotFoundException e ){
+        }
+    }
+
+    @Test
+    public void return_values () {
+        try{
+            assertThat(canThrow(intSneakilyThrows(), FileNotFoundException.class), Matchers.is(1));
+        }catch( FileNotFoundException e ){
+        }
+    }
+
+    @Test
+    public void void_methods () {
+        try{
+            canThrow( () -> sneakilyThrows(), FileNotFoundException.class);
+        }catch( FileNotFoundException e ){
+        }
+    }
+
+    @Test
+    public void try_catch_constructor () {
+        try( Rethrow canThrow = new Rethrow(FileNotFoundException.class) ){
             sneakilyThrows();
-            Assert.fail();
-        }catch( IOException e ){
+        }catch( FileNotFoundException e ){
+        }
+    }
+
+    @Test
+    public void try_catch_method () {
+        try( Rethrow canThrow = Rethrow.canThrow(FileNotFoundException.class) ){
+            sneakilyThrows();
+        }catch( FileNotFoundException e ){
         }
     }
 
     @SneakyThrows
     private void sneakilyThrows () {
-        throw new IOException();
+        try{
+            return;
+        }catch( Exception e ){
+            throw new FileNotFoundException();
+        }
+    }
+
+    @SneakyThrows
+    private int intSneakilyThrows () {
+        try{
+            return 1;
+        }catch( Exception e ){
+            throw new FileNotFoundException();
+        }
+    }
+
+    @SneakyThrows
+    private void loudlyThrows () throws FileNotFoundException {
+        throw new FileNotFoundException();
     }
 
 }
