f99404f5a370e08d4989766d58b5a92fcff5a81e
frigocoder@gmail.com <frigocoder@gmail.com>
2017-10-22 17:40:10 +0200

    Added Rethrow.canThrow overrides for primitive types.

--- src/main/java/frigo/lang/Rethrow.java
+++ src/main/java/frigo/lang/Rethrow.java
@@ -11,6 +11,38 @@ public class Rethrow implements AutoCloseable {
         return result;
     }
 
+    public static <E extends Exception> boolean canThrow (boolean result, Class<E> e) throws E {
+        return result;
+    }
+
+    public static <E extends Exception> char canThrow (char result, Class<E> e) throws E {
+        return result;
+    }
+
+    public static <E extends Exception> byte canThrow (byte result, Class<E> e) throws E {
+        return result;
+    }
+
+    public static <E extends Exception> short canThrow (short result, Class<E> e) throws E {
+        return result;
+    }
+
+    public static <E extends Exception> int canThrow (int result, Class<E> e) throws E {
+        return result;
+    }
+
+    public static <E extends Exception> long canThrow (long result, Class<E> e) throws E {
+        return result;
+    }
+
+    public static <E extends Exception> float canThrow (float result, Class<E> e) throws E {
+        return result;
+    }
+
+    public static <E extends Exception> double canThrow (double result, Class<E> e) throws E {
+        return result;
+    }
+
     public static <E extends Exception> void canThrow (Runnable runnable, Class<E> e) throws E {
         runnable.run();
     }
