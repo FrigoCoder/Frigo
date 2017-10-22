3ca4690e3e05e996c02cb08dd4f7c94c615478d4
frigocoder@gmail.com <frigocoder@gmail.com>
2017-10-22 17:54:24 +0200

    Merged Convoluter and Resampler into Sampler.

--- src/main/java/frigo/filter/Sampler.java
+++ src/main/java/frigo/filter/Sampler.java
@@ -7,6 +7,7 @@ import static frigo.math.integer.MathAux.floor;
 public class Sampler {
 
     private static final double SOURCE_SAMPLE_CENTER = 0.5;
+    private static final double DESTINATION_SAMPLE_CENTER = 0.5;
 
     private Kernel kernel;
 
@@ -47,6 +48,38 @@ public class Sampler {
 
     private double mapKernelToSource (double center, double scale, double location) {
         return center + location * scale;
+    }
+
+    public double[] convolute (double[] source) {
+        return convolute(source, 1.0);
+    }
+
+    public double[] convolute (double[] source, double scale) {
+        double[] result = new double[source.length];
+        for( int i = 0; i < result.length; i++ ){
+            result[i] = sample(source, i + SOURCE_SAMPLE_CENTER, scale);
+        }
+        return result;
+    }
+
+    public double[] resample (double[] source, int length) {
+        double[] destination = new double[length];
+        double location = mapDestinationIndexToSource(length, source.length, 0);
+        double increment = mapDestinationIndexToSource(length, source.length, 1) - location;
+        double scale = getScale(source.length, length);
+        for( int index = 0; index < destination.length; index++ ){
+            destination[index] = sample(source, location, scale);
+            location += increment;
+        }
+        return destination;
+    }
+
+    private double getScale (int sourceLength, int destinationLength) {
+        return Math.max((double) sourceLength / (double) destinationLength, 1.0);
+    }
+
+    private double mapDestinationIndexToSource (int destinationLength, int sourceLength, int index) {
+        return (index + DESTINATION_SAMPLE_CENTER) / destinationLength * sourceLength;
     }
 
 }
--- src/test/java/frigo/filter/SamplerTest.java
+++ src/test/java/frigo/filter/SamplerTest.java
@@ -1,6 +1,7 @@
 
 package frigo.filter;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -31,4 +32,32 @@ public class SamplerTest {
         assertThat(sampler.sample(source, 0.5, 0.0), is(0.0));
     }
 
+    @Test
+    public void convolute_scale_one_should_return_original_array () {
+        assertThat(sampler.convolute(source), equalTo(source));
+    }
+
+    @Test
+    public void convolute_scale_two_should_return_blurred_array () {
+        double[] expected = {2, 4.5, 9.5, 16.5, 25.5, 32.333333333333333333333333333333};
+        assertThat(sampler.convolute(source, 2.0), equalTo(expected));
+    }
+
+    @Test
+    public void resample_same_size () {
+        assertThat(sampler.resample(source, source.length), equalTo(source));
+    }
+
+    @Test
+    public void resample_half_size () {
+        double[] expected = {3.4285714285714284, 13.0, 28.428571428571427};
+        assertThat(sampler.resample(source, expected.length), equalTo(expected));
+
+    }
+
+    @Test
+    public void resample_double_size () {
+        double[] expected = {1.0, 1.75, 3.25, 5.25, 7.75, 10.75, 14.25, 18.25, 22.75, 27.75, 33.25, 36.0};
+        assertThat(sampler.resample(source, expected.length), equalTo(expected));
+    }
 }
